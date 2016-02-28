ef687ac257ca43e85969c8b7ccc96b93af27e068
Enghui Yap <enghuiy@gmail.com>
2016-02-28 18:53:00 -0500

    test

--- .#test.py
+++
@@ -1,1 +0,0 @@
-enghuiy@enghuiMac.home.4111
--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect
 import requests
-import pandas as pd
+#import pandas as pd
 from bokeh.plotting import figure
 from bokeh.embed import components 
 
@@ -24,14 +24,17 @@ def index():
     data=zip(*[line.split(',') for line in rtext])
 
     # put into a pandas dataframe
-    df=pd.DataFrame({'date':pd.to_datetime(data[0]),
-                 'price':pd.to_numeric(data[1])})
-    df['price']=df['price'].astype('float')
+#    df=pd.DataFrame({'date':pd.to_datetime(data[0]),
+#                 'price':pd.to_numeric(data[1])})
+#    df['price']=df['price'].astype('float')
 
     # plot with bokeh
-    plot = figure(width=450, height=450, y_axis_label='Stock Price',
-                x_axis_label='date', x_axis_type='datetime')
-    plot.line(df['date'],df['price'],color='green',line_width=3)
+    #plot = figure(width=450, height=450, y_axis_label='Stock Price', x_axis_label='date', x_axis_type='datetime')
+    #plot.circle([1,2,3,4],[1,1,1,1],color='green')
+    plot = figure()
+    plot.circle([1,2], [3,4])
+
+    #plot.line(df['date'],df['price'],color='green',line_width=3)
 
     script, div = components(plot)
     return render_template('graph.html', ticker=ticker, script=script, div=div)
@@ -39,4 +42,5 @@ def index():
   return render_template('index.html')
 
 if __name__ == '__main__':
-  app.run(port=33507, debug=True)
+#  app.run(port=33507)
+  app.run(debug=True)
