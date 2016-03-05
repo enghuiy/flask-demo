78ea9d362a2400a5f4bd0ccaeef4344b5c1acefc
Enghui Yap <enghuiy@gmail.com>
2016-03-05 08:03:58 -0500

    add pandas

--- app.py
+++ app.py
@@ -29,10 +29,6 @@ def index():
 
     # plot with bokeh
     plot = figure(width=450, height=450, y_axis_label='Stock Price', x_axis_label='date', x_axis_type='datetime')
-    #plot.circle([1,2,3,4],[1,1,1,1],color='green')
-    #plot = figure()
-    #plot.circle([1,2], [3,4])
-
     plot.line(df['date'],df['price'],color='green',line_width=3)
 
     script, div = components(plot)
@@ -41,5 +37,5 @@ def index():
   return render_template('index.html')
 
 if __name__ == '__main__':
-#  app.run(port=33507)
-  app.run(debug=True)
+  app.run(port=33507)
+#  app.run(debug=True)
